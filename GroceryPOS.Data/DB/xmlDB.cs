0e446a53b7a2d47eed402b01731c3c9703dc1e8f
Nguyen Nguyen <nltn@msn.com>
2017-09-13 16:50:33 -0700

    Change dbPath variable from private to public

---
+++ GroceryPOS.Data/DB/xmlDB.cs
@@ -0,0 +1,77 @@
+﻿//  ******************************************************
+//  ** Orange Coast College                             **
+//  ** Fall Semester 2017                               **
+//  ** CS A140 - Introduction to C# .NET Programming    **
+//  ** CRN          :   26637                           **
+//  ** Project name :   Grocery POS                     **
+//  ** Group members:   Brendan Carpio, Youlim Kim,     **
+//  **                  Christian Ayala, Ben Nguyen,    **
+//  **                  Nguyen Nguyen                   **
+//  ******************************************************
+
+//  DB\xmlDB.cs
+//  https://github.com/NLTN/GroceryPOS.git
+//  Version 0.0.1
+//  Created by Nguyen Nguyen on 9/1/2017.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace GroceryPOS.Data.DB
+{
+    public class xmlDB
+    {
+        #region Constructor - Singleton
+        private static xmlDB _instance;
+
+        private xmlDB() { loadDatabase(); }
+
+        public static xmlDB Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new xmlDB();
+                }
+                return _instance;
+            }
+        }
+        #endregion
+
+        #region Public Variables
+        public static XDocument doc;
+        #endregion
+
+        #region Private Variables
+        /// <summary>
+        /// Database Path
+        /// </summary>
+        public static string dbPath = Environment.CurrentDirectory + "\\db.xml";
+        #endregion
+
+        #region Private Functions
+        /// <summary>
+        /// Load XML file
+        /// </summary>
+        public static void loadDatabase()
+        {
+            Console.WriteLine("DB path = " + dbPath);
+            doc = XDocument.Load(dbPath);
+            //Console.WriteLine("Number of elements: " + doc.Element("products").Elements().Count());
+        }
+
+        /// <summary>
+        /// Save to XML file
+        /// </summary>
+        public static void SaveChanges()
+        {
+            doc.Save(dbPath);
+        }
+        #endregion
+    }
+}
---
+++ GroceryPOS.Data/Models/Order.cs
@@ -0,0 +1,32 @@
+﻿//  ******************************************************
+//  ** Orange Coast College                             **
+//  ** Fall Semester 2017                               **
+//  ** CS A140 - Introduction to C# .NET Programming    **
+//  ** CRN          :   26637                           **
+//  ** Project name :   Grocery POS                     **
+//  ** Group members:   Brendan Carpio, Youlim Kim,     **
+//  **                  Christian Ayala, Ben Nguyen,    **
+//  **                  Nguyen Nguyen                   **
+//  ******************************************************
+
+//  Models\Order.cs
+//  https://github.com/NLTN/GroceryPOS.git
+//  Version 0.0.1
+//  Created by Nguyen Nguyen on 9/1/2017.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryPOS.Models
+{
+    public class Order
+    {
+        public string OrderID { get; set; }
+        public DateTime Datetime { get; set; }
+
+        public OrderDetail[] Details { get; set; }
+    }   
+}
