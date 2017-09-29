aa425a5fc37a76b9e995b3f3c22f1fd71e4d3408
Nguyen Nguyen <nltn@msn.com>
2017-09-29 00:02:31 -0700

    Update ProductBLL.Add
    Auto generate ID if needed

--- GroceryPOS.Data/BLL/ProductBLL.cs
+++ GroceryPOS.Data/BLL/ProductBLL.cs
@@ -84,6 +84,10 @@ namespace GroceryPOS.Data.BLL
         /// <param name="imgPath">Image path</param>
         public static void Add(string id, string name, double price, string imagePath)
         {
+            // Auto generate an ID if id is empty
+            if (id == string.Empty) {
+                id = GenerateID();
+            }
             new DAL.ProductDAL().Add(id, name, price, imagePath);
         }
 
