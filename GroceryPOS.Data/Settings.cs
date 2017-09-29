5efe290f1f4e70f72a9a18b4f11d3a6c609b8f62
Nguyen Nguyen <nltn@msn.com>
2017-09-29 16:30:45 -0700

    Add a Settings class

---
+++ GroceryPOS.Data/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GroceryPOS.Data
+{
+    public class Settings
+    {
+        #region Private Fields
+        public static string StorageDirectory = System.Environment.CurrentDirectory + "/Storage";
+        #endregion
+    }
+}
