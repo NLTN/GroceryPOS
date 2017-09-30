2e5097711d34d05e2ed5e9577e39fe71ffc70e2c
Nguyen Nguyen <nltn@msn.com>
2017-09-29 17:13:41 -0700

    Update ProductUC design

--- GroceryPOS/SampleForms/ProductUserControl.Designer.cs
+++ GroceryPOS/SampleForms/ProductUserControl.Designer.cs
@@ -37,28 +37,31 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::GroceryPOS.Properties.Resources.PhotoPlaceholder;
-            this.pictureBox1.Location = new System.Drawing.Point(27, 16);
+            this.pictureBox1.Location = new System.Drawing.Point(3, 24);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 83);
+            this.pictureBox1.Size = new System.Drawing.Size(131, 99);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // lblName
             // 
-            this.lblName.Location = new System.Drawing.Point(27, 106);
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(3, 0);
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(100, 23);
+            this.lblName.Size = new System.Drawing.Size(131, 21);
             this.lblName.TabIndex = 1;
-            this.lblName.Text = "label1";
+            this.lblName.Text = "Product Name";
+            this.lblName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // lblPrice
             // 
-            this.lblPrice.Location = new System.Drawing.Point(24, 127);
+            this.lblPrice.Location = new System.Drawing.Point(3, 126);
             this.lblPrice.Name = "lblPrice";
-            this.lblPrice.Size = new System.Drawing.Size(100, 23);
+            this.lblPrice.Size = new System.Drawing.Size(131, 23);
             this.lblPrice.TabIndex = 2;
-            this.lblPrice.Text = "label1";
+            this.lblPrice.Text = "10.55";
+            this.lblPrice.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // ProductUserControl
             // 
@@ -68,6 +71,7 @@
             this.Controls.Add(this.lblName);
             this.Controls.Add(this.pictureBox1);
             this.Name = "ProductUserControl";
+            this.Size = new System.Drawing.Size(137, 152);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
